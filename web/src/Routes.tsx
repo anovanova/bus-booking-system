589a90a91780ab37dc59516ed289e2f749fba6d0
macedera <michael.acedera@fasttrackph.com>
2024-09-02 22:15:42 +0800

    chore(): change landing page route path

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -12,7 +12,7 @@ import { Router, Route } from '@redwoodjs/router'
 const Routes = () => {
   return (
     <Router>
-      <Route path="/landing" page={LandingPage} name="landing" />
+      <Route path="/" page={LandingPage} name="landing" />
       <Route notfound page={NotFoundPage} />
     </Router>
   )
