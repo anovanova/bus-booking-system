5cf1c724d2a5918d81a37a0d4db729abf7c02d5d
macedera <michael.acedera@fasttrackph.com>
2024-10-21 22:38:50 +0800

    feat(): add button on landing page

--- web/src/pages/LandingPage/LandingPage.tsx
+++ web/src/pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,7 @@
-import { Link, routes } from '@redwoodjs/router'
+import { navigate, routes } from '@redwoodjs/router'
 import { Metadata } from '@redwoodjs/web'
+
+import { Button } from 'src/components/ui/button'
 
 const LandingPage = () => {
   return (
@@ -12,12 +14,14 @@ const LandingPage = () => {
             This system helps you to book a bus to your destination!
           </p>
           <div className="h-44 grid place-content-center">
-            <Link
-              to={routes.personalInformation()}
-              className="btn btn-lg btn-primary m-auto"
+            <Button
+              onClick={() => {
+                navigate(routes.personalInformation())
+              }}
+              variant="default"
             >
               Book Now!
-            </Link>
+            </Button>
           </div>
         </div>
       </div>
